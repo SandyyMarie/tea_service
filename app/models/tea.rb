7b6de267b6cbfe351bcf613500183cb65a2fc075
Sandy Griffin <Sandra.Griffin@colorado.edu>
2023-01-12 13:03:57 -0700

    feat: completed validation testing, adding factorybot, and adding seeded faker data

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -2,6 +2,6 @@ class Tea < ApplicationRecord
   has_many :subscriptions
   has_many :customers, through: :subscriptions
   
-  validates_presence_of :first_name, :last_name, :email, :address
+  validates_presence_of :title, :description, :temperature, :brew_time
 
 end
--- spec/factories/factory_bot.rb
+++ spec/factories/factory_bot.rb
@@ -11,14 +11,13 @@ FactoryBot.define do
     description { Faker::Tea.type }
     temperature { Faker::Number.within(range: 80..200) }
     brew_time { Faker::Number.within(range: 1..10) }
-    
   end
 
   factory :subscriptions, class: Subscription do
     association :customers, factory: :customer
     association :teas, factory: :tea
     title { "Tea Subscription # #{Faker::Number.between(from: 1, to: 100)}" }
-    price { Faker::Number.between(from: 1.0, to: 15.00) }
+    price { Faker::Number.between(from: 1.0, to: 15.00).round(2) }
     status { Faker::Boolean.boolean }
     frequency { Faker::Number.between(from: 3, to: 30) }
   end
